02b43732e5fdcb9e980f24995174ea1909d99dab
Bipin Prasad <bprasad@yahooinc.com>
2023-04-10 22:08:49 -0700

    Do not publish from github action.

--- project.clj
+++ project.clj
@@ -7,7 +7,8 @@
   :java-source-paths ["src/jvm"]
   :javac-options ["-source" "1.8" "-target" "1.8"]
   :description "Write Clojure data to and from bytes using Kryo."
-  ;;:repositories {"mvncentral" "https://repo1.maven.org/maven2/"}
+  :repositories [["central" {:url "https://repo1.maven.org/maven2/"}]
+                 ["clojars" {:url "https://clojars.org/repo/"}]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [com.esotericsoftware/kryo "4.0.2"]
                  [com.twitter/chill-java "0.9.5"]]
@@ -21,13 +22,24 @@
   :developers [{:name "Bipin Prasad"
                 :email "bipinprasad@apache.org"
                 :roles [:developer]}]
-  :deploy-repositories {"releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-                                    :username :env/MVN_USERNAME
-                                    :password :env/MVN_PASSWORD}
-                        "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
-                                     :username :env/MVN_USERNAME
-                                     :password :env/MVN_PASSWORD}}
+;  :deploy-repositories {"releases" {:url "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+;                                    :username :env/MVN_USERNAME
+;                                    :password :env/MVN_PASSWORD}
+;                        "snapshots" {:url "https://oss.sonatype.org/content/repositories/snapshots/"
+;                                     :username :env/MVN_USERNAME
+;                                     :password :env/MVN_PASSWORD}}
+  :deploy-repositories {"releases" {:url "https://clojars.org/repo/"
+                                    :username :env/CLOJARS_USERNAME
+                                    :password :env/CLOJARS_API_KEY
+                                    :sign-releases true  ;; set this to true and run lein deploy from Mac so the jar/pom can be signed
+                                    }
+                        "snapshots" {:url "https://clojars.org/repo/"
+                                     :username :env/CLOJARS_USERNAME
+                                     :password :env/CLOJARS_API_KEY
+                                     :snapshot true
+                                     }}
   :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}})
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
+  )
