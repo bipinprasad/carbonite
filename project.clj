1041e8ff527ccbdd4ffa65c7d0aa15f82d6d0175
Bipin Prasad <bprasad@yahooinc.com>
2023-04-10 19:18:32 -0700

    Update package to org.apache.storm JVM to 1.8, Kryo to 4.0.2, chill-java to 0.9.5, and url link to new github location

--- project.clj
+++ project.clj
@@ -1,13 +1,19 @@
-(defproject com.twitter/carbonite "1.5.0"
+(defproject org.apache.storm/carbonite "1.6.0"
   :source-paths ["src/clj"]
   :java-source-paths ["src/jvm"]
-  :javac-options ["-source" "1.6" "-target" "1.6"]
+  :javac-options ["-source" "1.8" "-target" "1.8"]
   :description "Write Clojure data to and from bytes using Kryo."
-  :repositories {"conjars" "http://conjars.org/repo/"}
+  ;;:repositories {"mvncentral" "https://repo1.maven.org/maven2/"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.esotericsoftware/kryo "3.0.3"]
-                 [com.twitter/chill-java "0.8.0"]]
-  :global-vars {*warn-on-reflection* true}
+                 [com.esotericsoftware/kryo "4.0.2"]
+                 [com.twitter/chill-java "0.9.5"]]
+  :scm {:url "https://github.com/bipinprasad/carbonite"
+        :connection "scm:git:git://github.com/bipinprasad/carbonite.git"
+        :developer {:name "Bipin Prasad"
+                    :email "bipinprasad@apache.org"}}
+  :developers [{:name "Bipin Prasad"
+                :email "bipinprasad@apache.org"
+                :roles [:developer]}]
   :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
              :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
